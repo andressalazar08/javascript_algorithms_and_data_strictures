136354bb16c289c27f6f9317662c379446a5e513
AndresSalazar <andres.f.salazar@correounivalle.edu.co>
2022-12-19 16:49:55 -0500

    introducción

--- Seccion_2_ Big_O_Notation/Intro_1.js
+++ Seccion_2_ Big_O_Notation/Intro_1.js
@@ -12,5 +12,31 @@ function addUpTo(n){
     return total;
 }
 
-console.log(addUpTo(6));
-console.log(addUpTo(5));
+//console.log(addUpTo(6));
+//console.log(addUpTo(5));
+
+//Segunda forma
+
+function addUpTo_2(n){
+    return (n*(n+1))/2;
+}
+
+
+
+//console.log(addUpTo_2(6));
+//console.log(addUpTo_2(5));
+
+
+//Midiendo el desempeño
+
+var time1 = performance.now()
+addUpTo(1000000000);
+var time2 = performance.now()
+console.log(`Tiempo de ejecución algoritmo 1: ${(time2-time1)/1000} segundos.`)
+
+
+
+var time3 = performance.now()
+addUpTo_2(1000000000);
+var time4 = performance.now()
+console.log(`Tiempo de ejecución algoritmo 2: ${(time3-time4)/1000} segundos.`)
